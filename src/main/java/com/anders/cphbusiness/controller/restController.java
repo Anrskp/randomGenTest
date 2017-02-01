bd44e64cf63769eb94548891fa85092eae535f8e
Anders Kaae Pedersen <ekankp@danskespil.dk>
2017-02-01 09:34:09 +0100

    quickfix

--- src/main/java/com/anders/cphbusiness/controller/restController.java
+++ src/main/java/com/anders/cphbusiness/controller/restController.java
@@ -1,12 +1,14 @@
 package com.anders.cphbusiness.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.anders.cphbusiness.repositories.wagerBoardMarksRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.anders.cphbusiness.entitiesModel.*;
+import com.anders.cphbusiness.numbersModel.testSomething;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -16,6 +18,11 @@ public class restController {
     // FIELDS
     @Autowired
     private wagerBoardMarksRepo repo;
+
+    private ArrayList<Integer> randomNumbers = new ArrayList<>();
+    private ArrayList<wagerBoardMarks> seq = new ArrayList<>();
+    private ArrayList<java.util.Date> dates = new ArrayList<>();
+
     private randomTestController testCtrl = new randomTestController();
 
     // CONSTRUCTOR
@@ -24,11 +31,9 @@ public class restController {
 
     // METHODS
     @RequestMapping("/rngCheck")
-    public boolean testFeedback() {
-
-        ArrayList<Integer> randomNumbers = new ArrayList<>();
-        ArrayList<wagerBoardMarks> seq = new ArrayList<>();
-        ArrayList<java.util.Date> dates = new ArrayList<>();
+    public
+    @ResponseBody
+    testSomething rngCheck() {
 
         for (wagerBoardMarks wagerBoardMarks : repo.findAll()) {
             seq.add(wagerBoardMarks);
@@ -38,12 +43,20 @@ public class restController {
 
         // get sample dates
         Collections.sort(dates);
-        System.out.println("sample from : " + dates.get(0) + " to " + dates.get(dates.size() - 1));
+        Date fromDate = dates.get(0);
+        Date toDate = dates.get(dates.size() - 1);
 
-        boolean runsTestRes = testCtrl.runsTest(randomNumbers).isTestConclusion();
-        boolean occurrencesTestRes = (testCtrl.occurrencesTest(randomNumbers));
-        boolean boardSeqTestRes = testCtrl.boardSeqTest(seq);
+        // check date format compared to json.
+        System.out.println("from date: " + fromDate);
+        System.out.println("to date: " + toDate);
 
-        return runsTestRes && occurrencesTestRes && boardSeqTestRes;
+        boolean testResults = testCtrl.runsTest(randomNumbers).isTestConclusion()
+                && testCtrl.occurrencesTest(randomNumbers)
+                && testCtrl.boardSeqTest(seq);
+
+        if (testResults) {
+            return new testSomething(true, "Success", fromDate, toDate);
+        } else
+            return new testSomething(false, "Failure", fromDate, toDate);
     }
 }
